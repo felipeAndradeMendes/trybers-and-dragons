e75f462601d93949b4af42fbe4f9629c930a904c
Felipe Mendes <felipemadruga@gmail.com>
2023-07-10 18:25:39 -0300

    Inicia implementação requisito 07

---
+++ src/Character.ts
@@ -0,0 +1,68 @@
+import Archetype from './Archetypes';
+import Race from './Races';
+import Fighter from './Fighter';
+import Energy from './Energy';
+import getRandomInt from './utils';
+import Elf from './Races/Elf';
+import Mage from './Archetypes/Mage';
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+  private _name: string;
+
+  constructor(name: string) {
+    this._name = name;
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(this._name, this._dexterity);
+    this._archetype = new Mage('Mago1');
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = { 
+      type_: this._archetype.energyType, amount: getRandomInt(1, 10), 
+    };
+  }
+
+  // GETTERS
+  get name(): string {
+    return this._name;
+  }
+
+  get race(): Race {
+    return this._race;
+  }
+
+  get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  get energy(): Energy {
+    return this._energy;
+  }
+}
+
+const car1 = new Character('Felipe');
--- src/Energy.ts
+++ src/Energy.ts
@@ -1,6 +1,6 @@
 export type EnergyType = 'mana' | 'stamina';
 
 export default interface Energy {
-  type_: EnergyType;
-  amount: number;
+  readonly type_: EnergyType;
+  readonly amount: number;
 }
